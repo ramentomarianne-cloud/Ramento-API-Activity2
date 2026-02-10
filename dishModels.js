16e772329d35757fd8f8f0fc001c9bf19567f7b4
ramentomarianne-cloud <ramento.marianne@marsu.edu.ph>
2026-02-10 14:46:27 +0800

    Update dishModels.js

--- dishModels.js
+++ dishModels.js
@@ -3,6 +3,8 @@ const dishSchema = new mongoose.Schema({
     name: { type: String, 
         required: true,
         unique: true,
+        min: [0, 'Price cannot be negative'],
+        max: [1000, 'Price cannot exceed 1000'],
      },
     price: { type: Number, 
         required: true 
@@ -14,6 +16,18 @@ const dishSchema = new mongoose.Schema({
     isVegetarian: { 
         type: Boolean,
         default: false,
-    }
+    },
+    reviews: [
+        {
+            user: {String,
+            ratings: {type: Number, min: 1, max:5},
+            comment: String,}
+        }
+    ],
+    chef: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Chef',
+    },
 });
+
 module.exports = mongoose.model('Dish', dishSchema);
