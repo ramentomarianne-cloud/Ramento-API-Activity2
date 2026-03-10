d0830d1b29fb673d1c4730b4545e20b563a26f7d
ramentomarianne-cloud <ramento.marianne@marsu.edu.ph>
2026-03-10 21:14:52 +0800

    Update apiRoutes.js

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -15,6 +15,9 @@ router.post('/dishes', createDish);
 router.get('/dishes/:id', getDishById);
 router.put('/dishes/:id', updateDish);
 router.delete('/dishes/:id', deleteDish);
+const { protect, authorize } = require('../middleware/authMiddleware');
+router.get('/', getDishes);
+router.post('/', protect, authorize('admin', 'manager'), createDish);
 module.exports = router;
 
 
