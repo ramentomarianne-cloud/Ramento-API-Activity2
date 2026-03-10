ec544ee335ac02eba5f8389388dc21c25bea983a
Your Name <you@example.com>
2026-03-10 13:13:18 +0800

    Update

--- chefModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const chefSchema = new mongoose.Schema({
-    name: String,
-    specialty: String,
-    });
-    
-module.exports = mongoose.model('Chef', chefSchema);
---
+++ src/routes/apiRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+const {
+    getAllDishes,
+    createDish,
+    getDishById,
+    updateDish,
+    deleteDish, 
+} = require('../controllers/dishController');
+
+router.get('/dishes', getAllDishes);
+router.post('/dishes', createDish);
+router.get('/dishes/:id', getDishById);
+router.put('/dishes/:id', updateDish);
+router.delete('/dishes/:id', deleteDish);
+module.exports = router;
+
+
+
